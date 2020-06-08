4503a9e2c91a2aa05734c810c96b4aae3b9cf8f1
aptx17 <41042297+aptx17@users.noreply.github.com>
2020-06-09 06:55:38 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://git.openwrt.org/openwrt/openwrt.git
-cd openwrt0-r2S
-patch -p1 < ../PATCH/R.patch
+cd openwrt
+patch -p1 < ../PATCH/R2S.patch
 cd target/linux/rockchip
 mkdir patches-5.4
 cp -rf ../../../../PATCH/000-add-nanopi-r2s-support.patch patches-5.4/000-add-nanopi-r2s-support.patch
